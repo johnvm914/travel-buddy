cb12115174adcc43c066aab68b073e3449b87f58
John Messina <johnvm914@gmail.com>
2017-04-14 11:37:30 -0400

    removed comma from s3 credentials

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,7 +7,7 @@ class Profile < ApplicationRecord
                       bucket: ENV['S3_BUCKET_NAME'],
                       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                      s3_region: ENV['AWS_REGION'],
+                      s3_region: ENV['AWS_REGION']
                     }
   validates_attachment :profile_pic, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   belongs_to :user
