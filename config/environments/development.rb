422c68e86ef3eed63d6547488952b0eb42475619
John Messina <johnvm914@gmail.com>
2017-04-14 13:04:38 -0400

    added s3 credentials to development.rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,17 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: ENV['AWS_HOST_NAME'],
+    s3_region: ENV['AWS_REGION'],
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
