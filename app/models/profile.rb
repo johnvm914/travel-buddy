a359d72e3d72a763a3e6e1b7b5f92670da69ecdc
John Messina <johnvm914@gmail.com>
2017-04-14 11:44:52 -0400

    changed s3 credentials order

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,11 +3,11 @@ class Profile < ApplicationRecord
                     styles: { thumb: ["250x250#", :jpg] },
                     storage: :s3,
                     s3_host_name: ENV['AWS_HOST_NAME'],
+                    s3_region: ENV['AWS_REGION'],
                     s3_credentials: {
                       bucket: ENV['S3_BUCKET_NAME'],
                       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                      s3_region: ENV['AWS_REGION']
+                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                     }
   validates_attachment :profile_pic, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   belongs_to :user
